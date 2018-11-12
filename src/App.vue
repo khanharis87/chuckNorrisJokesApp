de96199b7367f54c60fe07ba09c4a9dec601e93c
Haris Khan <haris.khan87@gmail.com>
2018-11-12 12:57:13 +0100

    initial app

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="container">
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      title: "Chcuk Norries Jokes"
+    };
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  width: 600px;
+  margin: 50px auto;
+  text-align: center;
+}
+</style>
